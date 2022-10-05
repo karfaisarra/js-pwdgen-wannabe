c83441549a24cebe4f8da67bfb3cbfeb10b6c7b2
karfaisarra <karfaisarra@gmail.com>
2022-10-05 14:04:21 +0200

    add commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const num= 22;
+//console.log(num);
+//Chiedi all’utente il suo nome,
+const first_name = prompt('Inserisci il tuo nome');
+//console.log(first_name);
+
+//poi chiedi il suo cognome
+const last_name = prompt('Inserisci il tuo cognome');
+//console.log(last_name);
+
+//poi chiedi il suo colore preferito
+const color = prompt('Inserisci il tuo colore preferito');
+//console.log(color);
+
+//Infine scrivi sulla pagina nomecognomecolorepreferito22
+document.getElementById('password').innerHTML =`la tua password sicura è : ${first_name}${last_name}${color}${num}`;
